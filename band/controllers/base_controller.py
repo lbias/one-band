f4a130abb8d11f984e28d0f3aefa34d6c0994013
samzhuwj <samzhuwj@gmail.com>
2017-11-23 14:20:57 +0800

    controller, link to handle the sign up request

--- band/controllers/base_controller.py
+++ band/controllers/base_controller.py
@@ -2,6 +2,7 @@ import pyramid.httpexceptions as exc
 import pyramid.renderers
 
 import band.infrastructure.static_cache as static_cache
+from band.infrastructure.supressor import suppress
 
 
 class BaseController:
@@ -17,7 +18,17 @@ class BaseController:
     def is_logged_in(self):
         return False
 
+    @suppress()
     def redirect(self, to_url, permanent=False):
         if permanent:
             raise exc.HTTPMovedPermanently(to_url)
         raise exc.HTTPFound(to_url)
+
+    @property
+    def data_dict(self):
+        data = dict()
+        data.update(self.request.GET)
+        data.update(self.request.POST)
+        data.update(self.request.matchdict)
+
+        return data
---
+++ band/controllers/newsletter_controller.py
@@ -0,0 +1,24 @@
+import pyramid_handlers
+
+from band.controllers.base_controller import BaseController
+from band.services.mailinglist_service import MailingListService
+
+
+class NewsletterController(BaseController):
+    # POST /newsletter/add_subscriber
+    @pyramid_handlers.action(request_method='POST')
+    def add_subscriber(self):
+        email = self.data_dict.get('email')
+
+        if MailingListService.add_subscriber(email):
+            self.redirect('/newsletter/subscribed')
+
+        self.redirect('/newsletter/failed')
+
+    @pyramid_handlers.action(renderer='templates/newsletter/subscribed.pt')
+    def subscribed(self):
+        return {}
+
+    @pyramid_handlers.action(renderer='templates/newsletter/failed.pt')
+    def failed(self):
+        return {}
