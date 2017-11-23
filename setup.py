923e85423769966ef22267bf6d342312cd71b6e8
samzhuwj <samzhuwj@gmail.com>
2017-11-23 23:21:07 +0800

    set up notifications in Rollbar

--- setup.py
+++ setup.py
@@ -9,48 +9,50 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'html2text',
+    'logbook',
     'mailchimp',
-    'passlib',    
-    'plaster_pastedeploy',
+    'mailer',
+    'passlib',
     'pyramid',
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'pyramid_handlers',
-    'waitress',
+    'rollbar',
     'sqlalchemy',
+    'stripe',
+    'waitress',
 ]
 
 tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest',
+    'WebTest >= 1.3.1',  # py3 compatibility
+    'pytest',  # includes virtualenv
     'pytest-cov',
 ]
 
-setup(
-    name='band',
-    version='0.0',
-    description='band',
-    long_description=README + '\n\n' + CHANGES,
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-    ],
-    author='',
-    author_email='',
-    url='',
-    keywords='web pyramid pylons',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
-    install_requires=requires,
-    entry_points={
-        'paste.app_factory': [
-            'main = band:main',
-        ],
-    },
-)
+setup(name='band',
+      version='0.0',
+      description='band',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = band:main
+      """,
+      )
