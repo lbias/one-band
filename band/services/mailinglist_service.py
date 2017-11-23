b41595a116dfbb7dd0b9da833e0776e0b0df603c
samzhuwj <samzhuwj@gmail.com>
2017-11-23 17:15:46 +0800

    use the MailChimp API

--- band/services/mailinglist_service.py
+++ band/services/mailinglist_service.py
@@ -12,4 +12,25 @@ class MailingListService:
 
     @staticmethod
     def add_subscriber(email):
-        pass
+        if (MailingListService.mailchimp_api == 'THE_API_KEY' or
+                    MailingListService.mailchimp_list_id == 'THE_LIST_ID'):
+            print("WARNING: CANNOT USE MAILCHIMP API. KEYS NOT SET")
+            print("Set your API keys in development.ini")
+            return False
+
+        api = mailchimp.Mailchimp(apikey=MailingListService.mailchimp_api)
+
+        if not email or not email.strip():
+            return False
+
+        try:
+            api.lists.subscribe(
+                MailingListService.mailchimp_list_id,
+                {'email': email.strip().lower()},
+                double_optin=False,
+                update_existing=True,
+                replace_interests=False)
+            return True
+        except Exception as x:
+            print("Error during mailing list signup: {}".format(x))
+            return False
